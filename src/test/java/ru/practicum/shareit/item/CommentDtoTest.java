cd475630136f34ca786f8b092164065bab150a7f
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-08-20 11:21:18 +0300

    fix: исправить недочеты

--- src/test/java/ru/practicum/shareit/item/CommentDtoTest.java
+++ src/test/java/ru/practicum/shareit/item/CommentDtoTest.java
@@ -18,7 +18,7 @@ class CommentDtoTest {
 
     @Autowired
     private JacksonTester<CommentDto> json;
-    private static final String TIME_PATTERN_TEST = "yyyy-MM-dd'T'HH:mm:ss.SSSSS";
+    private static final String TIME_PATTERN_TEST = "yyyy-MM-dd HH:mm:ssssss";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIME_PATTERN_TEST);
 
     @Test
